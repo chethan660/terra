bb9fb9db1e4266b0eea740347eebbbe334afe729
shetty <shetty@gmail.com>
2023-09-03 16:32:45 +0530

    update

--- ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "kube_worker" {
-    ami = data.aws_ami.node_ami.id
-    instance_type = "t2.micro"
-    key_name = "docker"                #it uses docker key to install the instance
-    tags = {
-        Name = "node 2"
-    }
-}
